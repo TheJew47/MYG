917fc01063b7cbf34652de4da0d5289d1bcae9b3
TheJew47 <241754437+TheJew47@users.noreply.github.com>
2025-12-27 12:47:31 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,11 +3,12 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/backend-api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        // Frontend path you will call in your code
+        source: '/api/v1/:path*', 
+        // The actual HTTP destination on EC2
+        destination: 'http://3.216.174.254:8000/:path*', 
       },
     ]
   },
 }
-
 module.exports = nextConfig
